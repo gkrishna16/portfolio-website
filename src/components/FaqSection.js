40d220de3b8a688cc2ae2cb7c9b5f8c63f3e69ac
Gopal <gkrishna160@gmail.com>
2021-02-15 18:55:25 -0500

    4.FAQ Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I Start ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>
+            Lorem Ipsum,dolor sit amet consectetur adipiscing Necessitatibus,
+            neque
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>How do I Start ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>
+            Lorem Ipsum,dolor sit amet consectetur adipiscing Necessitatibus,
+            neque
+          </p>
+        </div>
+      </div>{" "}
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>
+            Lorem Ipsum,dolor sit amet consectetur adipiscing Necessitatibus,
+            neque
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 export default function AboutUs() {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+      <FaqSection />
+    </>
   );
 }
